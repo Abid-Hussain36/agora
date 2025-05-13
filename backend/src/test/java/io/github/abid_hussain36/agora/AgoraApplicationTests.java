625abfc06d5af5a3ec08b143b297dd2b6709bf4f
Abid Hussain <abid.geek@outlook.com>
2025-05-12 21:41:26 -0400

    Setup Backend

---
+++ backend/src/test/java/io/github/abid_hussain36/agora/AgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.abid_hussain36.agora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
